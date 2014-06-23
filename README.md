3721cf71e02398469fbcb945205cc014109947d6
wangcheng <wangcheng@baidu.com>
2014-06-23 19:55:50 +0800

    add fis pm

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 fis-pm
-======
+================
+
+## 安装
+
+* npm install -g fis-pm
+
+## 使用
+
+* fis-pm monit start //监控pm2的子进程内存状况，放置内存泄漏
+* fis-pm monit stop //停止pm2子进程内存监控
+* fis-pm monit restart //重启pm2子进程内存监控
---
+++ fis-pm.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var fispm = module.exports;
+
+Object.defineProperty(global, 'fispm', {
+    enumerable : true,
+    writable : false,
+    value : fispm
+});
+
+fispm.util = require("./lib/util.js");
+
+fispm.cli = {};
+fispm.cli.name = "fis-pm";
+fispm.cli.commander = null;
+fispm.cli.info = fispm.util.readJSON(__dirname + "/package.json");
+
+fispm.cli.help = function(){
+    console.log(fispm.cli.info);
+}
+
+fispm.cli.version = function(){
+    console.log(fispm.cli.info.version);   
+}
+
+fispm.cli.run = function(argv){
+    var first = argv[2];
+
+    if(first === '-h' || first === '--help'){
+        fispm.cli.help();
+    } else if(first === '-v' || first === '--version') {
+        fispm.cli.version();
+    } else if(first[0] === '-'){
+        fispm.cli.help();
+    } else {
+        var commander = fispm.cli.commander = require('commander');
+        var cmd = require('./lib/' + first + '.js');
+        cmd.registry(
+            commander
+                .command(cmd.name || first)
+                .usage(cmd.usage)
+                .description(cmd.desc)
+        );
+        commander.parse(argv);
+    }
+};
